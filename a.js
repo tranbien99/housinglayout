15f262ab621337e8e18e89e4ac5abb2f9d70706d
TranBien99 <tranbien230199@gmail.com>
2022-01-08 09:50:50 +0700

    update

---
+++ a.js
@@ -0,0 +1,9 @@
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").animate.name = "example2";
+  document.getElementById("myModal").style.display = "none"; 
+}
+
